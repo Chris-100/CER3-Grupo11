85b0f363c194065c5460a4b88836e21cbae680e6
Chris-100 <christopher.huerta.castro@gmail.com>
2022-12-11 19:36:47 -0300

    tablasHistorial

--- prototipo/home/models.py
+++ prototipo/home/models.py
@@ -7,9 +7,17 @@ class Recien_nacido(models.Model):
     apellidos = models.CharField(max_length=30)
     rut = models.CharField(max_length=12)
     cupo = models.IntegerField()
-    edad = models.IntegerField()
     peso = models.FloatField()
     genero = models.CharField(max_length=12)
+
+class Historial_medico(models.Model):
+    temperatura = models.FloatField()
+    estado = models.CharField(max_length=30)
+    medicamento = models.CharField(max_length=300)
+    via_alimentacion = models.CharField(max_length=30)
+    tipo_alimentacion = models.CharField(max_length=30)
+    edad = models.IntegerField()
+    Rn = models.ForeignKey(Recien_nacido, on_delete=models.CASCADE)
 
 
 
