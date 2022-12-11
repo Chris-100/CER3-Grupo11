a1b28cf3dcf66ebb62070a6f89b27cba8340e098
Chris-100 <christopher.huerta.castro@gmail.com>
2022-12-11 19:20:48 -0300

    tablaRN

--- prototipo/home/models.py
+++ prototipo/home/models.py
@@ -1,3 +1,15 @@
-#from django.db import models
+from django.db import models
 
 # Create your models here.
+
+class Recien_nacido(models.Model):
+    name = models.CharField(max_length=30)
+    apellidos = models.CharField(max_length=30)
+    rut = models.CharField(max_length=12)
+    cupo = models.IntegerField()
+    edad = models.IntegerField()
+    peso = models.FloatField()
+    genero = models.CharField(max_length=12)
+
+
+
